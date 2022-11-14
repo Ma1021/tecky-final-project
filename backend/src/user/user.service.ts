433f1f458fbfe6bba5dd315bfe0cc53b59ed912c
Maggie Ng <maggienwn@gmail.com>
2022-11-14 17:42:49 +0800

    update gender to enum M F

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { hash } from 'bcrypt';
+import knex from 'knex';
+import { Interface } from 'readline';
+
+interface NewUser {
+  username: string;
+  dob: Date;
+}
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    // return 'This action adds a new user';
+    await knex('users').insert;
   }
 
   findAll() {

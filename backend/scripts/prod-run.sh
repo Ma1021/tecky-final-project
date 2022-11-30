9e28cb339ab6bc7792abf86750b79c66f2400907
Maggie Ng <maggienwn@gmail.com>
2022-11-30 16:19:47 +0800

    docker details

--- backend/scripts/prod-run.sh
+++ backend/scripts/prod-run.sh
@@ -3,7 +3,8 @@ set -e
 set -o pipefail
 cd dist
 # Manually might be better
-npx knex migrate:latest --env production
-npx knex seed:run
+# npx knex migrate:latest --env production
+# npx knex seed:run
+
 cd ../
 node ./dist/src/main.js

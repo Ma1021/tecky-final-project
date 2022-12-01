fd3382010a9c73782ce519f78984dd6b4b57f264
a1235456 <log38044@gmail.com>
2022-12-01 12:37:40 +0800

    added individual account preview with dynamic color

--- frontend/src/components/stock/IndividualAccountModule.tsx
+++ frontend/src/components/stock/IndividualAccountModule.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import AccountOverviewModule from "./AccountOverviewModule";
+import UserPositionModule from "./UserPositionModule";
 
 interface IndividualAccountModuleProps {
   category: string;
@@ -31,25 +32,49 @@ const IndividualAccountModule: React.FC<IndividualAccountModuleProps> = ({
   });
 
   useEffect(() => {
-    setAccountDetail({
-      totalAmount: 1000000,
-      todayProfit: 0,
-      todayProfitPercentage: 0,
-      marketValue: 0,
-      buyingPower: 1000000,
-      rank: 0,
-      totalProfit: 0,
-      totalProfitPercentage: 0,
-    });
+    if (category === "US") {
+      setAccountDetail({
+        totalAmount: 1000000,
+        todayProfit: 0,
+        todayProfitPercentage: 0,
+        marketValue: 0,
+        buyingPower: 1000000,
+        rank: 0,
+        totalProfit: 0,
+        totalProfitPercentage: 0,
+      });
+    } else if (category === "HK") {
+      setAccountDetail({
+        totalAmount: 2000000,
+        todayProfit: 1000,
+        todayProfitPercentage: 10,
+        marketValue: 1000000,
+        buyingPower: 1000000,
+        rank: 1,
+        totalProfit: 1000000,
+        totalProfitPercentage: 100,
+      });
+    } else {
+      setAccountDetail({
+        totalAmount: 10000,
+        todayProfit: -8000,
+        todayProfitPercentage: -80,
+        marketValue: 10000,
+        buyingPower: 0,
+        rank: 9999,
+        totalProfit: -9000,
+        totalProfitPercentage: -90,
+      });
+    }
   }, []);
 
   return (
     <>
-      <h1>{category}</h1>
       <AccountOverviewModule
         category={category}
         accountDetail={accountDetail}
       />
+      <UserPositionModule />
     </>
   );
 };

311fe4fc6bf96e4c7ec2e9eb0ba536ec49dffbaa
a1235456 <log38044@gmail.com>
2022-12-03 01:12:59 +0800

    modified order panel

--- frontend/src/pages/paperTrade/OrderPanel.tsx
+++ frontend/src/pages/paperTrade/OrderPanel.tsx
@@ -8,17 +8,19 @@ import {
   IonSelect,
   IonSelectOption,
 } from "@ionic/react";
-import React, { useState } from "react";
+import { useState } from "react";
+import "./OrderPanel.css";
 
 const OrderPanel: React.FC = () => {
   const [currentTicker, setCurrentTicker] = useState<string>("");
+  const [orderDirection, setOrderDirection] = useState("long");
   const [orderType, setOrderType] = useState("current");
 
   return (
     <>
       <IonList>
         <IonItem>
-          <IonLabel>代碼</IonLabel>
+          <IonLabel className="order-panel-label">代碼</IonLabel>
           <IonInput
             onIonChange={(e: any) => {
               setCurrentTicker(e.detail.value);
@@ -28,23 +30,37 @@ const OrderPanel: React.FC = () => {
         </IonItem>
 
         <IonItem>
-          <IonLabel>方向</IonLabel>
+          <IonLabel className="order-panel-label">方向</IonLabel>
           <IonButtons>
-            <IonButton>模擬買入</IonButton>
-            <IonButton>模擬賣出</IonButton>
+            <IonButton
+              className={orderDirection === "long" ? "long" : ""}
+              onClick={() => setOrderDirection("long")}
+            >
+              模擬買入
+            </IonButton>
+            <IonButton
+              className={orderDirection === "short" ? "short" : ""}
+              onClick={() => setOrderDirection("short")}
+            >
+              模擬賣出
+            </IonButton>
           </IonButtons>
         </IonItem>
 
         <IonItem>
-          <IonLabel>類型</IonLabel>
-          <IonSelect interface="popover" value={orderType} onIonChange={(e: any) => setOrderType(e.detail.value)}>
+          <IonLabel className="order-panel-label">類型</IonLabel>
+          <IonSelect
+            interface="popover"
+            value={orderType}
+            onIonChange={(e: any) => setOrderType(e.detail.value)}
+          >
             <IonSelectOption value="current">市價單</IonSelectOption>
             <IonSelectOption value="fix">限價單</IonSelectOption>
           </IonSelect>
         </IonItem>
 
         <IonItem>
-          <IonLabel>價格</IonLabel>
+          <IonLabel className="order-panel-label">價格</IonLabel>
           <IonInput></IonInput>
           <IonButtons>
             <IonButton>-</IonButton>
@@ -53,7 +69,7 @@ const OrderPanel: React.FC = () => {
         </IonItem>
 
         <IonItem>
-          <IonLabel>數量</IonLabel>
+          <IonLabel className="order-panel-label">數量</IonLabel>
           <IonInput></IonInput>
           <IonButtons>
             <IonButton>-</IonButton>
@@ -62,16 +78,14 @@ const OrderPanel: React.FC = () => {
         </IonItem>
 
         <IonItem>
-          <IonLabel>最大可買</IonLabel>
-          <IonLabel>100</IonLabel>
+          <IonLabel>最大可買 100</IonLabel>
         </IonItem>
 
         <IonItem>
-          <IonLabel>金額</IonLabel>
-          <IonLabel>10000</IonLabel>
+          <IonLabel>金額 10000</IonLabel>
         </IonItem>
 
-        <IonButton>模擬買入</IonButton>
+        <IonButton className="order-confirm-button">模擬買入</IonButton>
       </IonList>
       <h1>{currentTicker}</h1>
     </>

24b844d3de9520bd3d6b8c507b14ce66efba337f
Maggie Ng <maggienwn@gmail.com>
2022-12-03 19:59:28 +0800

    comment out console.log

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -31,7 +31,7 @@ export class UserController {
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
-    console.log('enter create user');
+    // console.log('enter create user');
     try {
       if (createUserDto.password !== createUserDto.rePassword) {
         throw new Error('密碼不一致');
@@ -91,7 +91,7 @@ export class UserController {
     // createChatroomDto is ""
     // ! createChatroomDto = true
     try {
-      console.log('entered upuate');
+      // console.log('entered upuate');
       let result;
       if (!!file) {
         let fileUpload = {
@@ -100,7 +100,7 @@ export class UserController {
           fileMimetype: file.mimetype,
         };
         let s3File = await this.s3Service.uploadFile(fileUpload);
-        console.log(s3File);
+        // console.log(s3File);
         if (s3File == 'error') {
           throw new Error('圖片發生錯誤, 請重新輸入');
         }
@@ -111,7 +111,7 @@ export class UserController {
         updateUserDto.icon = null;
         result = await this.userService.update(id, updateUserDto);
       }
-      console.log('controller update user', result);
+      // console.log('controller update user', result);
       return result;
     } catch (error) {
       throw new HttpException(error, HttpStatus.BAD_REQUEST);
@@ -143,7 +143,7 @@ export class UserController {
   @Post('/block')
   async getBlockList(@Body() userIdDto: UserIdDto) {
     let result = await this.userService.getBlockUser(userIdDto);
-    console.log('user controller, blocker list', result);
+    // console.log('user controller, blocker list', result);
     return result;
   }
 
@@ -151,7 +151,7 @@ export class UserController {
   @Post(':id/block')
   async block(@Param('id') id: number, @Body() userIdDto: UserIdDto) {
     let result = await this.userService.blockUser(id, userIdDto);
-    console.log('user controller block', result);
+    // console.log('user controller block', result);
     return result;
   }
 
@@ -159,7 +159,7 @@ export class UserController {
   @Post(':id/unblock')
   async unblock(@Param('id') id: number, @Body() userIdDto: UserIdDto) {
     let result = await this.userService.unBlockUser(id, userIdDto);
-    console.log('user controller unblock', result);
+    // console.log('user controller unblock', result);
     return result;
   }
 

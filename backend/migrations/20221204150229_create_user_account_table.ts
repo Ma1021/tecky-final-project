bfcfc69c0f96b7cace889cf413f6237d91e58d96
a1235456 <log38044@gmail.com>
2022-12-08 01:33:26 +0800

    added user cryptos table and seed

--- backend/migrations/20221204150229_create_user_account_table.ts
+++ backend/migrations/20221204150229_create_user_account_table.ts
@@ -21,9 +21,18 @@ export async function up(knex: Knex): Promise<void> {
     table.timestamp('order_complete_time').defaultTo(knex.fn.now());
     table.string('account');
   });
+
+  await knex.schema.createTable('user_cryptos', (table) => {
+    table.increments('id', { primaryKey: true });
+    table.integer('user_id').references('users.id');
+    table.string('symbol');
+    table.string('name');
+    table.string('chinese_name');
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTableIfExists('user_paper_trade_accounts');
   await knex.schema.dropTableIfExists('trade_records');
+  await knex.schema.dropTableIfExists('user_cryptos');
 }
